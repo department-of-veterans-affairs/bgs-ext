207a67c490c5d8bf59130c649f5709ba77d8652a
Derek Dyer <derek@digitalderek.com>
2020-03-16 17:33:51 -0400

    Add fix for bgs

--- lib/lighthouse_bgs/services/vnp_ptcpnt.rb
+++ lib/lighthouse_bgs/services/vnp_ptcpnt.rb
@@ -9,33 +9,60 @@
 module LighthouseBGS
   class VnpPtcpntService < LighthouseBGS::Base
     def bean_name
-      'VnpProcFormWebServiceBean'
+      'VnpPtcpntServiceBean'
     end
 
     def self.service_name
       'vnp_ptcpnt'
     end
 
-    def vnp_ptcpnt_create(ssn:)
+    def vnp_ptcpnt_create(
+      vnp_ptcpnt_id:,
+      vnp_proc_id:,
+      fraud_ind:,
+      jrn_dt:,
+      jrn_lctn_id:,
+      jrn_obj_id:,
+      jrn_status_type_cd:,
+      jrn_user_id:,
+      legacy_poa_cd:,
+      misc_vendor_ind:,
+      ptcpnt_short_nm:,
+      ptcpnt_type_nm:,
+      tax_idfctn_nbr:,
+      tin_waiver_reason_type_cd:,
+      ptcpnt_fk_ptcpnt_id:,
+      corp_ptcpnt_id:,
+      ssn:
+      )
+
       response = request(
         :vnp_ptcpnt_create,
         {
           "arg0": {
-            "compId": {
-              "vnpProcId": vnp_proc_id,
-              "formTypeCd": form_type_cd
-            },
+            "vnpPtcpntId": vnp_ptcpnt_id,
+            "vnpProcId": vnp_proc_id,
+            "fraudInd": fraud_ind,
             "jrnDt": jrn_dt,
             "jrnLctnId": jrn_lctn_id,
             "jrnObjId": jrn_obj_id,
             "jrnStatusTypeCd": jrn_status_type_cd,
-            "jrnUserId": jrn_user_id
-          },
-          ssn
-        }
+            "jrnUserId": jrn_user_id,
+            "legacyPoaCd": legacy_poa_cd,
+            "miscVendorInd": misc_vendor_ind,
+            "ptcpntShortNm": ptcpnt_short_nm,
+            "ptcpntTypeNm": ptcpnt_type_nm,
+            "taxIdfctnNbr": tax_idfctn_nbr,
+            "tinWaiverReasonTypeCd": tin_waiver_reason_type_cd,
+            "ptcpntFkPtcpntId": ptcpnt_fk_ptcpnt_id,
+            "corpPtcpntId": corp_ptcpnt_id
+          }
+        },
+        ssn
       )
 
       response.body[:vnp_ptcpnt_create][:return]
     end
   end
 end
+

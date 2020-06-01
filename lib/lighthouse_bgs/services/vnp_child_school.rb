05a9bd1b5bdf93966f2c8e7d28310f725b5f76d5
Derek Dyer <derek@digitalderek.com>
2020-06-01 12:53:12 -0400

    Ensure all files have necessary comments

--- lib/lighthouse_bgs/services/vnp_child_school.rb
+++ lib/lighthouse_bgs/services/vnp_child_school.rb
@@ -1,3 +1,11 @@
+# frozen_string_literal: true
+
+# As a work of the United States Government, this project is in the
+# public domain within the United States.
+#
+# Additionally, we waive copyright and related rights in the work
+# worldwide through the CC0 1.0 Universal public domain dedication.
+
 module LighthouseBGS
   class VnpChildSchoolService < LighthouseBGS::Base
     def bean_name

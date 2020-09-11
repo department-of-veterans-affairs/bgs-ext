389d261995906254061a178dbeb5a932d74e3acd
Kathleen Crawford <kcrawford@governmentcio.com>
2020-09-11 09:37:35 -0700

    adding routing service

---
+++ lib/bgs/services/regional_office_routing.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+# As a work of the United States Government, this project is in the
+# public domain within the United States.
+#
+# Additionally, we waive copyright and related rights in the work
+# worldwide through the CC0 1.0 Universal public domain dedication.
+
+module BGS
+  class RORoutingService < BGS::Base
+    def bean_name
+      'RORoutingServiceWS'
+    end
+
+    def self.service_name
+      'routing'
+    end
+
+    # Finds regional office based on Veteran's zip code
+    def get_regional_office_by_zip_code()
+      response = request(
+        :get_regional_office_by_zip_code,
+        {
+          "searchROCriteria": {
+            "zipCode": '92651',
+            "country": 'US',
+            "province": ''
+          },
+          "lob": 'CP'
+        },
+        '333224444'
+      )
+      response.body[:get_regional_office_by_zip_code_response][:return]
+    end
+  end
+end

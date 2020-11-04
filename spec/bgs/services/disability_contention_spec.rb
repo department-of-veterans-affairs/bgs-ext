5b6165ef04e0a123c8fed8ef826f531aeee19ebe
Lee DeBoom <lee.deboom@oddball.io>
2020-11-04 17:01:41 -0600

    API-3117

--- spec/bgs/services/disability_contention_spec.rb
+++ spec/bgs/services/disability_contention_spec.rb
@@ -12,13 +12,13 @@ describe BGS::DisabilityContentionService do
 
   it 'get read_all_contentions' do
     VCR.use_cassette('disability_contention/read_all_contentions') do
-      response = service.disability_contention.read_all_contentions(13367440)
+      service.disability_contention.read_all_contentions(13367440)
     end
   end
 
   it 'get read_all_display_disabilities' do
     VCR.use_cassette('disability_contention/read_all_display_disabilities') do
-      response = service.disability_contention.read_all_display_disabilities(13367440)
+      service.disability_contention.read_all_display_disabilities(13367440)
     end
   end
 end

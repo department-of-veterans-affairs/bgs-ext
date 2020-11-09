181078ba5ae3220f8ca85fa96b88ad515e4f943a
camerontesterman <cameronbtesterman@gmail.com>
2020-11-09 11:32:54 -0600

    find vet awards methods added

--- lib/bgs/services/awards.rb
+++ lib/bgs/services/awards.rb
@@ -15,9 +15,28 @@ module BGS
       'AwardWebServiceBean'
     end
 
+    def self.service_name
+      'awards'
+    end
+
     def find_by_participant_id(participant_id)
       response = request(:find_award_bene_by_ptcpnt_vet_id, 'ptcpntVetId': participant_id)
       response.body[:findAwardBeneByPtcpntVetIdResponse]
+    end
+
+    def find_award_by_participant_id(participant_id)
+      response = request(:find_veteran_award_cmpsit_by_ptcpnt_id, 'ptcpntId': participant_id)
+      response.body[:find_veteran_award_cmpsit_by_ptcpnt_id_response][:return]
+    end
+
+    def find_award_by_ssn(ssn)
+      response = request(:find_veteran_award_cmpsit_by_ssn, 'ssn': ssn)
+      response.body[:find_veteran_award_cmpsit_by_ssn_response][:return]
+    end
+
+    def find_award_by_file_number(file_number)
+      response = request(:find_veteran_award_cmpsit_by_file_number, 'fileNumber': file_number)
+      response.body[:find_veteran_award_cmpsit_by_file_number_response][:return]
     end
   end
 end

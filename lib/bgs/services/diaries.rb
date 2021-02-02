72ee9286b9bab6d97ba49d2d9f9b45c83d39b113
Derek Dyer <derek@digitalderek.com>
2021-02-02 08:40:46 -0500

    Add diaries (#56)

---
+++ lib/bgs/services/diaries.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+module BGS
+  # This service is used to store information about individuals the VA is
+  # interested in. This information may be kept permanently, removed or discarded
+  # if appropriate.
+  class DiariesService < BGS::Base
+    def bean_name
+      'benefits-awards'
+    end
+
+    def namespace
+      { namespaces: { 'xmlns:data' => 'http://gov.va.vba.benefits.awards.ws/data' } }
+    end
+
+    def self.service_name
+      'diaries'
+    end
+
+    def read_diaries(options)
+      response = request(
+        :read_diaries,
+        {
+          'data:AwardKeyInput': {
+            'awardType': 'CPL',
+            'beneficiaryID': options[:participant_id],
+            'veteranID': options[:participant_id]
+          }
+        },
+        options[:ssn]
+      )
+
+      response.body[:read_diaries_response][:diary_response]
+    end
+  end
+end
---
+++ spec/bgs/services/diaries_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'bgs'
+
+describe BGS::DiariesService do
+  let(:service) do
+    BGS::Services.new(
+      external_uid: 'something',
+      external_key: 'something'
+    )
+  end
+
+  let(:diary_params) { { participant_id: 'something', ssn: 'something' } }
+
+  describe '#read_diaries' do
+    it 'returns vet diary information' do
+      VCR.use_cassette('diaries/read_diaries') do
+        response = service.diaries.read_diaries(diary_params)
+
+        expect(response).to include({ error_level: '0' })
+      end
+    end
+  end
+end

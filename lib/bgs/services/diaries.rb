3f012b7b1a5416c536657ccd04dac4c9ece68b49
Derek Dyer <derek@digitalderek.com>
2021-02-02 12:22:02 -0500

    Add updates

--- lib/bgs/services/diaries.rb
+++ lib/bgs/services/diaries.rb
@@ -32,5 +32,40 @@ module BGS
 
       response.body[:read_diaries_response][:diary_response]
     end
+
+    def update_diaries(options)
+      response = request(
+        :update_diaries,
+        {
+          'data:AwardKeyInput': {
+            'awardType': 'CPL',
+            'beneficiaryID': options[:benficiary_id],
+            'veteranID': options[:participant_id]
+          },
+          'data:DiaryInput': {
+            'awardType': 'CPL',
+            'beneficaryID': options[:benficiary_id],
+            'diaryDueDate': options[:diary_due_date],
+            'diaryLcStatusType': options[:diary_lc_status_type], # PEND
+            'diaryLcStatusTypeDescription': options[:diary_lc_status_description], # Pending
+            'diaryReasonType': options[:diary_reason_type], # 24
+            'diaryReasonTypeDescription': options[:diary_reason_type_description], #Issue Dependency Verification Form
+            'fileNumber': options[:file_number],
+            'firstNm': options[:first_name],
+            'lastName': options[:last_name],
+            'modifiedAction': options[:modified_action], # I
+            'modifiedBy': options[:modified_by], # CAPSBRAN
+            'modifiedDate': options[:modified_date],
+            'modifiedLocation': options[:modified_location], # 317
+            'modifiedProcess': options[:modified_process], # cp_diary_pkg.do_create
+            'ptcpntDiaryID': options[:participant_id],
+            'statusDate': options[:status_date],
+            'veteranID': options[:veteran_id]
+          }
+        }
+      )
+
+      response.body[:update_diaries_response][:diary_response]
+    end
   end
 end
--- spec/bgs/services/diaries_spec.rb
+++ spec/bgs/services/diaries_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'bgs'
+require 'pry'
 
 describe BGS::DiariesService do
   let(:service) do
@@ -10,14 +11,46 @@ describe BGS::DiariesService do
     )
   end
 
-  let(:diary_params) { { participant_id: 'something', ssn: 'something' } }
+  let(:read_diary_params) { { participant_id: 'something', ssn: 'something' } }
+  let(:update_diary_params) do
+    {
+      benficiary_id: '13014883',
+      diary_due_date: Time.now.utc.iso8601,
+      diary_lc_status_type: 'PEND',
+      diary_lc_status_description: 'Pending',
+      diary_reason_type: '24',
+      diary_reason_type_description: 'Issue Dependency Verification Form',
+      file_number: '546212222',
+      first_name: 'Abraham',
+      last_name: 'Lincoln',
+      modified_action: 'I', # I
+      modified_by: 'CAPSBRAN',
+      modified_date: Time.now.utc.iso8601,
+      modified_location: '317', # 317
+      modified_process: 'cp_diary_pkg.do_create', # cp_diary_pkg.do_create
+      participant_id: '13018359',
+      status_date: Time.now.utc.iso8601,
+      veteran_id: '13014883'
+    }
+  end
 
   describe '#read_diaries' do
     it 'returns vet diary information' do
       VCR.use_cassette('diaries/read_diaries') do
-        response = service.diaries.read_diaries(diary_params)
+        response = service.diaries.read_diaries(read_diary_params)
 
         expect(response).to include({ error_level: '0' })
+      end
+    end
+  end
+
+  describe '#update_diaries' do
+    it 'updates diaries' do
+      VCR.use_cassette('diaries/update_diaries') do
+        response = service.diaries.update_diaries(update_diary_params)
+
+        expect(response).to include({ error_level: '0' })
+        expect(response.dig(:diaries, :diary, :award_diary_id)).to be_truthy
       end
     end
   end

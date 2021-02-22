4eba200159ceff1b4df0fe8d4c406ff41c18598b
Derek Dyer <derek@digitalderek.com>
2021-02-22 11:46:37 -0500

    Add update diaries (#57)

--- lib/bgs/services/diaries.rb
+++ lib/bgs/services/diaries.rb
@@ -22,8 +22,8 @@ module BGS
         :read_diaries,
         {
           'data:AwardKeyInput': {
-            'awardType': 'CPL',
-            'beneficiaryID': options[:participant_id],
+            'awardType': options[:award_type],
+            'beneficiaryID': options[:beneficiary_id],
             'veteranID': options[:participant_id]
           }
         },
@@ -32,5 +32,52 @@ module BGS
 
       response.body[:read_diaries_response][:diary_response]
     end
+
+    def update_diaries(options, diaries)
+      response = request(
+        :update_diaries, {
+          'data:AwardKeyInput' => {
+            'awardType' => options[:award_type],
+            'beneficiaryID' => options[:beneficiary_id],
+            'veteranID' => options[:participant_id]
+          },
+          'data:DiaryInput' => formatted_diary_list(diaries)
+        }
+      )
+
+      response.body[:update_diaries_response][:diary_response]
+    end
+
+    def formatted_diary_list(diaries)
+      diaries.map do |diary|
+        diary_hash(diary)
+      end
+    end
+
+    # rubocop:disable Metrics/MethodLength
+    def diary_hash(diary)
+      {
+        'awardDiaryID' => diary[:award_diary_id],
+        'awardType' => diary[:award_type],
+        'beneficaryID' => diary[:beneficary_id],
+        'diaryDueDate' => diary[:diary_due_date],
+        'diaryLcStatusType' => diary[:diary_lc_status_type],
+        'diaryLcStatusTypeDescription' => diary[:diary_lc_status_type_description],
+        'diaryReasonType' => diary[:diary_reason_type],
+        'diaryReasonTypeDescription' => diary[:diary_reason_type_description],
+        'fileNumber' => diary[:file_number],
+        'firstNm' => diary[:first_nm],
+        'lastName' => diary[:last_name],
+        'modifiedAction' => diary[:modified_action],
+        'modifiedBy' => diary[:modified_by],
+        'modifiedDate' => diary[:modified_date],
+        'modifiedLocation' => diary[:modified_location],
+        'modifiedProcess' => diary[:modified_process],
+        'ptcpntDiaryID' => diary[:ptcpnt_diary_id],
+        'statusDate' => diary[:status_date],
+        'veteranID' => diary[:veteran_id]
+      }
+    end
+    # rubocop:enable Metrics/MethodLength
   end
 end
--- spec/bgs/services/diaries_spec.rb
+++ spec/bgs/services/diaries_spec.rb
@@ -2,6 +2,7 @@
 
 require 'bgs'
 
+# rubocop:disable Metrics/BlockLength
 describe BGS::DiariesService do
   let(:service) do
     BGS::Services.new(
@@ -10,15 +11,101 @@ describe BGS::DiariesService do
     )
   end
 
-  let(:diary_params) { { participant_id: 'something', ssn: 'something' } }
+  let(:read_diary_params) { { participant_id: 'something', ssn: 'something' } }
+  let(:update_diary_params) do
+    [
+      {
+        award_diary_id: '71829',
+        award_type: 'CPL',
+        beneficary_id: '13014883',
+        diary_due_date: '2014-05-01T00:00:00-05:00',
+        diary_lc_status_type: 'PEND',
+        diary_lc_status_type_description: 'Pending',
+        diary_reason_type: '24',
+        diary_reason_type_description: 'Issue Dependency Verification Form',
+        file_number: '546212222',
+        first_nm: 'Ray',
+        last_name: 'Jakes',
+        modified_action: 'I',
+        modified_by: 'VAgovAPI',
+        modified_date: '2021-02-02T12:12:32-06:00',
+        modified_location: '281',
+        modified_process: 'VAgovAPI',
+        ptcpnt_diary_id: '13018359',
+        status_date: '2021-02-02T12:12:32-06:00',
+        veteran_id: '13014883'
+      },
+      {
+        award_diary_id: '71830',
+        award_type: 'CPL',
+        beneficary_id: '13014883',
+        diary_due_date: '2014-05-01T00:00:00-05:00',
+        diary_lc_status_type: 'PEND',
+        diary_lc_status_type_description: 'Pending',
+        diary_reason_type: '24',
+        diary_reason_type_description: 'Issue Dependency Verification Form',
+        file_number: '546212222',
+        first_nm: 'Ray',
+        last_name: 'Jakes',
+        modified_action: 'I',
+        modified_by: 'VAgovAPI',
+        modified_date: '2021-02-02T12:12:32-06:00',
+        modified_location: '281',
+        modified_process: 'VAgovAPI',
+        ptcpnt_diary_id: '13018359',
+        status_date: '2021-02-02T12:12:32-06:00',
+        veteran_id: '13014883'
+      },
+      {
+        award_diary_id: '3323',
+        award_type: 'CPL',
+        beneficary_id: '13014883',
+        diary_due_date: '2015-05-01T00:00:00-05:00',
+        diary_lc_status_type: 'PEND',
+        diary_lc_status_type_description: 'Pending',
+        diary_reason_type: '24',
+        diary_reason_type_description: 'Issue Dependency Verification Form',
+        file_number: '546212222',
+        first_nm: 'Ray',
+        last_name: 'Jakes',
+        modified_action: 'U',
+        modified_by: 'VAgovAPI',
+        modified_date: '2021-02-02T12:12:31-06:00',
+        modified_location: '281',
+        modified_process: 'VAgovAPI',
+        ptcpnt_diary_id: '13018359',
+        status_date: '2006-05-02T08:32:18-05:00',
+        veteran_id: '13014883'
+      }
+    ]
+  end
+  let(:update_options) do
+    {
+      award_type: 'CPL',
+      beneficiary_id: '13014883',
+      participant_id: '13014883'
+    }
+  end
 
   describe '#read_diaries' do
     it 'returns vet diary information' do
       VCR.use_cassette('diaries/read_diaries') do
-        response = service.diaries.read_diaries(diary_params)
+        response = service.diaries.read_diaries(read_diary_params)
 
         expect(response).to include({ error_level: '0' })
       end
     end
   end
+
+  describe '#update_diaries' do
+    it 'updates diaries' do
+      VCR.use_cassette('diaries/update_diaries') do
+        response = service.diaries.update_diaries(update_options, update_diary_params)
+
+        expect(response).to include({ error_level: '0' })
+        expect(response.dig(:diaries, :diary).first).to be_truthy
+      end
+    end
+  end
 end
+# rubocop:enable Metrics/BlockLength

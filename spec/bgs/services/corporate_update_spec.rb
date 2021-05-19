a8ca45c2ed862331792367b7940fd61c655cc19a
Lee DeBoom <lee.deboom@oddball.io>
2021-05-19 10:20:11 -0500

    API-6461

--- spec/bgs/services/corporate_update_spec.rb
+++ spec/bgs/services/corporate_update_spec.rb
@@ -3,7 +3,7 @@
 require 'bgs'
 
 describe BGS::CorporateUpdateWebService do
-  let(:poa_code) { '091' }
+  let(:poa_code) { 'A1Q' }
   let(:participant_id) { '600043201' }
   let(:service) do
     BGS::Services.new(
@@ -12,14 +12,35 @@ describe BGS::CorporateUpdateWebService do
     )
   end
 
-  xit 'updates poa access' do
-    pending('Receiving tuxedo error when hitting endpoint')
-
+  it 'updates poa access' do
     VCR.use_cassette('corporate_update/update_poa_access') do
-      service.corporate_update.update_poa_access(
+      response = service.corporate_update.update_poa_access(
         participant_id: participant_id,
         poa_code: poa_code
       )
+
+      expect(response[:allow_poa_cadd]).to eq('y')
+      expect(response[:audit_related_ar]).to eq(nil)
+      expect(response[:award_bene_indicator]).to eq('$')
+      expect(response[:email_address]).to eq('mytest@hotmail.com')
+      expect(response[:file_number]).to eq('796130115')
+      expect(response[:first_name]).to eq('TAMARA')
+      expect(response[:gender]).to eq('F')
+      expect(response[:ig_identifier]).to eq(nil)
+      expect(response[:last_name]).to eq('ELLIS')
+      expect(response[:middle_name]).to eq('E')
+      expect(response[:poa]).to eq('A1Q')
+      expect(response[:poa_name]).to eq('A1Q - PATRICIA E ROBERTS')
+      expect(response[:ptcpnt_id]).to eq('600043201')
+      expect(response[:rcvbl_indicator]).to eq('Y')
+      expect(response[:return_code]).to eq('GUIE50000')
+      expect(response[:return_message]).to eq('Success')
+      expect(response[:sensitive]).to eq('U')
+      expect(response[:ssn]).to eq('796130115')
+      expect(response[:suffix]).to eq(nil)
+      expect(response[:tax_abatement_indicator]).to eq(nil)
+      expect(response[:title]).to eq(nil)
+      expect(response[:verified_ssn]).to eq('N')
     end
   end
 end

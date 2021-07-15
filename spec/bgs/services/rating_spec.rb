624904c52e699f13225010c26962a1d08560de96
Lee DeBoom <lee.deboom@oddball.io>
2021-07-15 14:03:12 -0500

    API-8525: Store example response for ratings BGS call

---
+++ spec/bgs/services/rating_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'bgs'
+require 'byebug'
+
+describe BGS::RatingService do
+  let(:service) do
+    BGS::Services.new(
+      external_uid: 'something',
+      external_key: 'something'
+    )
+  end
+
+  describe '#find_rating_data' do
+    it 'returns a valid regional office response' do
+      VCR.use_cassette('rating/find_rating_data') do
+        service.rating.find_rating_data('796123232')
+      end
+    end
+  end
+end
